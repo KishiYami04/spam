d4f38934e6c280be47aafc6fd01b4e187c0dbf09
KishiYami04 <80523633+KishiYami04@users.noreply.github.com>
2021-11-19 19:40:25 +0700

    spam_all

---
+++ start.py
@@ -0,0 +1,31 @@
+import time , sys , os
+os.system("clear")
+print("\033[1;96m===Welcome to Kishi Spam===")
+print("")
+print("1;96m====== Menu ======")
+print("\033[1;95m[1] \033[1;92mยิงเบอร์.v1")
+print("\033[1;95m[2] \033[1;92mยิงเบอร์v.2")
+print("\033[1;95m[3] \033[1;92mยิงเมล")
+print("\033[1;95m[4] \033[1;92mHack")
+print("\033[1;95m[00] \033[1;92mออก")
+print("")
+print("\033[1;91m[*] By: Aimbot Sorecode Time: 7/11/2021")
+print("")
+verfly = input("\033[90m> \033[1;97mEnter the attack : \033[1;96m")
+if verfly == '1':
+	os.system("python FunctionA.py")
+	
+if verfly == '2':
+	os.system("python FunctionB.py")
+	
+if verfly == '3':
+	os.system("exit")
+	
+if verfly == '4':
+	os.system("python FunctionC.py")
+	
+if verfly == '00':
+	print("\033[1;96mAimbot")
+	os.system("exit")
+
+    
